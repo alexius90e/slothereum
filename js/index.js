001cbb2122cb0ff440c8a478c98011e87b8661c7
Aliaksei Karzhou <alexius90e@gmail.com>
2025-02-11 15:05:13 +0300

    feat: added preloader

---
+++ js/index.js
@@ -0,0 +1,23 @@
+const preloaderWrapper = document.querySelector('.preloader__wrapper');
+const preloaderContent = document.querySelector('.preloader__content');
+const preloaderVideo = document.querySelector('.preloader__video');
+const screen = document.querySelector('.screen');
+
+if (preloaderWrapper && preloaderContent && preloaderVideo) {
+  preloaderWrapper.addEventListener('scroll', (event) => {
+    const contentHeight = preloaderContent.offsetHeight;
+    const videoHeight = preloaderVideo.offsetHeight;
+    const scrollPosition = event.target.scrollTop;
+    const percentPosition = Math.floor((scrollPosition / (contentHeight - videoHeight)) * 100);
+    const transformProp = `scale(${1 + (percentPosition * 1.4) / 100}) translateX(${
+      -percentPosition * 0.22
+    }%)`;
+    preloaderVideo.style.transform = transformProp;
+
+    if (percentPosition >= 70) {
+      // const screenScalePercent = ((percentPosition - 90) / 10) * 100;
+
+      if (screen) screen.classList.add('active');
+    }
+  });
+}
