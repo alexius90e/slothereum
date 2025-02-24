262c40bd657510420435eacbd7c9304d50e83288
Aliaksei Karzhou <alexius90e@gmail.com>
2025-02-24 15:05:33 +0300

    feat: added swipe-login

--- js/os.js
+++ js/os.js
@@ -4,6 +4,7 @@ const osSections = {
   authCode: 'auth-code',
   authCodeRef: 'auth-code_referral',
   authCodeEmail: 'auth-code_email',
+  swipeLogin: 'swipe-login',
 };
 
 const showOsSection = (sectionClassName) => {
@@ -119,4 +120,75 @@ authCodeElems.forEach((authCodeElem) => {
       }
     });
   });
+
+  if (continueButton)
+    continueButton.addEventListener('click', () => {
+      hideOsSection(osSections.authCodeEmail);
+      hideOsSection(osSections.authCodeRef);
+      showOsSection(osSections.swipeLogin);
+    });
+});
+
+/// swipe-login
+
+const swipeLoginSliders = document.querySelectorAll('.swipe-login__slider');
+
+swipeLoginSliders.forEach((slider) => {
+  const draggable = document.querySelector('.swipe-login__slider-draggable');
+  const track = document.querySelector('.swipe-login__slider-track');
+
+  let isDragging = false;
+  let startX = 0;
+  let draggableLeft = 0;
+  let maxLeft = 0;
+
+  draggable.addEventListener('mousedown', startDrag);
+  draggable.addEventListener('touchstart', startDrag);
+
+  function startDrag(e) {
+    isDragging = true;
+    startX = e.clientX || e.touches[0].clientX;
+    draggableLeft = draggable.offsetLeft;
+    maxLeft = slider.clientWidth * 0.6;
+
+    document.addEventListener('mousemove', drag);
+    document.addEventListener('touchmove', drag);
+    document.addEventListener('mouseup', endDrag);
+    document.addEventListener('touchend', endDrag);
+    document.addEventListener('contextmenu', (event) => event.preventDefault());
+    document.body.style.userSelect = 'none';
+  }
+
+  function drag(e) {
+    if (!isDragging) return;
+    const x = (e.clientX || e.touches[0].clientX) - startX;
+    let newLeft = draggableLeft + x;
+
+    newLeft = Math.max(0, Math.min(newLeft, maxLeft));
+
+    draggable.style.left = `${newLeft}px`;
+    track.style.width = `${newLeft + draggable.offsetWidth}px`;
+  }
+
+  function endDrag() {
+    if (!isDragging) return;
+    isDragging = false;
+
+    if (draggable.offsetLeft >= maxLeft) {
+      unlock();
+    } else {
+      draggable.style.left = '0';
+      track.style.width = '0';
+    }
+  }
+
+  function unlock() {
+    draggable.removeEventListener('mousedown', startDrag);
+    draggable.removeEventListener('touchstart', startDrag);
+    document.removeEventListener('mousemove', drag);
+    document.removeEventListener('touchmove', drag);
+    document.removeEventListener('contextmenu', (event) => event.preventDefault());
+    document.body.style.userSelect = '';
+    hideOsSection(osSections.swipeLogin)
+  }
 });
